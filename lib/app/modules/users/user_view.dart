4092d3dc15fb1e6c906fb6a83e63701d1b4837d2
Antonio Jr <antonio.jr@outlook.com>
2020-10-25 09:38:06 -0300

    User list in cards

--- lib/app/modules/users/user_view.dart
+++ lib/app/modules/users/user_view.dart
@@ -1,9 +1,25 @@
 import 'package:get/get_state_manager/get_state_manager.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:getx_modelo/app/data/model/user.dart';
 import 'package:getx_modelo/app/modules/users/user_controller.dart';
 
 class UserView extends GetView<UserController> {
+  Widget _buildCard(User user) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          ListTile(
+            leading: Icon(Icons.account_circle),
+            title: Text(user.name),
+            subtitle: Text(user.email),
+            tileColor: Colors.grey[200],            
+          ),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,9 +30,7 @@ class UserView extends GetView<UserController> {
             return ListView.builder(
               itemCount: _.users.length,
               itemBuilder: (context, index) {
-                return Container(
-                  child: Text(_.users[index].name),
-                );
+                return _buildCard(_.users[index]);
               },
             );
           },
